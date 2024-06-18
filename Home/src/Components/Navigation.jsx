2eaf9d39d105e16456327e554b902e28461471a4
Ghanindra <bohoraghanindra@gmail.com>
2024-06-18 21:52:46 +0545

    nike website

---
+++ Home/src/Components/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+        <nav className='container'>
+      <div class="logo">
+        <img src="Images/nikelogo.jpg" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+    <button>LOGIN</button>
+      </nav>
+
+    </div>
+  )
+}
+
+export default Navigation
---
+++ Home/src/User.jsx
@@ -0,0 +1,11 @@
+
+import { Profile } from './Profile.jsx';
+export default function User(){
+    return(
+        <div>
+  <h1> Hello Beautiful</h1>
+  <Profile />
+        </div>
+      
+    );
+}
